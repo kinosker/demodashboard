9eca4952655a793cc5c72b3e37e75cfc2ef071fb
Tien Long Kuan <tienlongz@gmail.com>
2026-03-18 13:17:33 +0800

    Demo ready

---
+++ modalCardData.js
@@ -0,0 +1,32 @@
+const staffProfiles = {
+  Alex: {
+    rank: "Staff Sergeant",
+    contingencyRole: "IC1",
+    joinDate: "20 March 2022",
+    upcomingDuties: [
+      { date: "21 March 2026",  type: "ph" },
+      { date: "7 April 2026",   type: "wd" },
+      { date: "15 April 2026",  type: "wd" },
+    ]
+  },
+  Alan: {
+    rank: "Corporal",
+    contingencyRole: "IC2",
+    joinDate: "5 January 2023",
+    upcomingDuties: [
+      { date: "22 March 2026",  type: "we" },
+      { date: "10 April 2026",  type: "wd" },
+      { date: "28 April 2026",  type: "we" },
+    ]
+  },
+  Mel: {
+    rank: "Sergeant",
+    contingencyRole: "2IC",
+    joinDate: "15 July 2021",
+    upcomingDuties: [
+      { date: "19 March 2026",  type: "wd" },
+      { date: "29 March 2026",  type: "we" },
+      { date: "18 April 2026",  type: "ph" },
+    ]
+  }
+};
