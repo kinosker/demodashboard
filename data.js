206fabc8d6cfcda140e9ba81fb4643e5500abc69
Tien Long Kuan <tienlongz@gmail.com>
2026-03-18 12:54:04 +0800

    - First working

---
+++ data.js
@@ -0,0 +1,71 @@
+const data = {
+  years: [
+    {
+      year: 2026,
+      staff: [
+        {
+          name: "Alex",
+          months: [
+            { name:"Jan", weekday:6, weekend:2, ph:0 },
+            { name:"Feb", weekday:6, weekend:2, ph:0 },
+            { name:"Mar", weekday:3, weekend:1, ph:0 },
+            { name:"Apr", weekday:2, weekend:1, ph:0 },
+            { name:"May", weekday:2, weekend:1, ph:1 },
+            { name:"Jun", weekday:1, weekend:1, ph:0 },
+          ]
+        },
+        {
+          name: "Alan",
+          months: [
+            { name:"Jan", weekday:7, weekend:1, ph:1 },
+            { name:"Feb", weekday:4, weekend:2, ph:0 },
+            { name:"Mar", weekday:3, weekend:1, ph:1 },
+            { name:"Apr", weekday:2, weekend:1, ph:0 },
+            { name:"May", weekday:1, weekend:1, ph:0 },
+          ]
+        },
+        {
+          name: "Mel",
+          months: [
+            { name:"Jan", weekday:6, weekend:2, ph:0 },
+            { name:"Feb", weekday:5, weekend:2, ph:2 },
+            { name:"Mar", weekday:4, weekend:2, ph:0 },
+            { name:"Apr", weekday:2, weekend:2, ph:1 },
+            { name:"May", weekday:2, weekend:1, ph:0 },
+            { name:"Jun", weekday:1, weekend:1, ph:1 },
+          ]
+        }
+      ]
+    },
+    {
+      year: 2025,
+      staff: [
+        {
+          name: "Alex",
+          months: [
+            { name:"Jan", weekday:5, weekend:2, ph:0 },
+            { name:"Feb", weekday:4, weekend:2, ph:0 },
+            { name:"Mar", weekday:3, weekend:1, ph:1 },
+            { name:"Apr", weekday:2, weekend:1, ph:0 },
+          ]
+        },
+        {
+          name: "Alan",
+          months: [
+            { name:"Jan", weekday:6, weekend:1, ph:1 },
+            { name:"Feb", weekday:4, weekend:2, ph:1 },
+            { name:"Mar", weekday:2, weekend:1, ph:0 },
+          ]
+        },
+        {
+          name: "Mel",
+          months: [
+            { name:"Jan", weekday:5, weekend:2, ph:0 },
+            { name:"Feb", weekday:4, weekend:1, ph:1 },
+            { name:"Apr", weekday:2, weekend:2, ph:0 },
+          ]
+        }
+      ]
+    }
+  ]
+};
